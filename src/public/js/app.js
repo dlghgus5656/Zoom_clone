f0e919ceee7376e4af6afe82f152aa826e1de888
dlghgus5656 <82634597+dlghgus5656@users.noreply.github.com>
2022-03-28 19:44:56 +0900

    SOCKETIO
    WebSocket 에서 SocketIO로 전환
    npm i socket.io
    socket.emit
    script(src="/socket.io/socket.io.js")

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,54 +1,73 @@
-// Front-End 부분
-// 여기서의 socket는 서버로의 연결을 뜻함
-const messageList = document.querySelector("ul");
-const nicknameForm = document.querySelector("#name");
-const messageForm = document.querySelector("#message");
-const socket = new WebSocket(`ws://${window.location.host}`);
+// // Front-End 부분
+const socket = io(); // 프론트에서 백으로 연결
 
-function makeMessage(type, payload) {
-  const msg = { type, payload };
-  return JSON.stringify(msg);
+const welcome = document.getElementById("welcome");
+const form = welcome.querySelector("form");
+
+// socketIO는 WebSocket처럼 오브젝트를 스트링으로 변환하는 등에 작업을 해줄 필요 없다.
+function handleRoomSubmit(event) {
+  event.preventDefault(); // 이벤트 고유기능 새로고침 등 막기
+  const input = form.querySelector("input");
+  socket.emit("enter_room", { payload: input.value }, () => {
+    //서버에서 done function을 실행 시키지만 실행 되는건 여기 프론트단에 있는 function이 실행 된다.
+    console.log("server is done!!!");
+  }); // socketIO 1. 특정한 event를 emit해 줄 수 있다. 이름 상관없이, 2. object를 전송할 수 있다.(string만 전송 할 필요 없음)
+  input.value = "";
 }
 
-// 아래 주석처리가 된 코드는 익명함수를 사용하지 않고 따로 function을 만들어 사용한 것이고
-// 주석처리가 안된 현재 코드는 익명함수를 사용한것이다.
-// function handleOpen() {
-//     console.log("Connected to Server ✅");
+form.addEventListener("submit", handleRoomSubmit);
+
+// webSocket로 한 부분
+// // 여기서의 socket는 서버로의 연결을 뜻함
+// const messageList = document.querySelector("ul");
+// const nicknameForm = document.querySelector("#name");
+// const messageForm = document.querySelector("#message");
+// const socket = new WebSocket(`ws://${window.location.host}`);
+
+// function makeMessage(type, payload) {
+//   const msg = { type, payload };
+//   return JSON.stringify(msg);
 // }
 
-// socket.addEventListener("open", handleOpen)
-socket.addEventListener("open", () => {
-  console.log("Connected to Server ✅");
-});
+// // 아래 주석처리가 된 코드는 익명함수를 사용하지 않고 따로 function을 만들어 사용한 것이고
+// // 주석처리가 안된 현재 코드는 익명함수를 사용한것이다.
+// // function handleOpen() {
+// //     console.log("Connected to Server ✅");
+// // }
 
-socket.addEventListener("message", (message) => {
-  // console.log("새 메세지: ", message.data, " 를 서버로 부터 받아왔습니다.");
-  const li = document.createElement("li");
-  li.innerText = message.data;
-  messageList.append(li);
-});
+// // socket.addEventListener("open", handleOpen)
+// socket.addEventListener("open", () => {
+//   console.log("Connected to Server ✅");
+// });
 
-socket.addEventListener("close", () => {
-  console.log("Disconnected from Server ❌");
-});
+// socket.addEventListener("message", (message) => {
+//   // console.log("새 메세지: ", message.data, " 를 서버로 부터 받아왔습니다.");
+//   const li = document.createElement("li");
+//   li.innerText = message.data;
+//   messageList.append(li);
+// });
 
-// setTimeout(() => {
-//   socket.send("hello from the browser!!");
-// }, 6000);
+// socket.addEventListener("close", () => {
+//   console.log("Disconnected from Server ❌");
+// });
 
-function handleSubmit(event) {
-  event.preventDefault();
-  const input = messageForm.querySelector("input");
-  socket.send(makeMessage("new_message", input.value));
-  input.value = "";
-}
+// // setTimeout(() => {
+// //   socket.send("hello from the browser!!");
+// // }, 6000);
 
-function handleNickSubmit(event) {
-  event.preventDefault();
-  const input = nicknameForm.querySelector("input");
-  socket.send(makeMessage("nickname", input.value));
-  input.value = "";
-}
+// function handleSubmit(event) {
+//   event.preventDefault();
+//   const input = messageForm.querySelector("input");
+//   socket.send(makeMessage("new_message", input.value));
+//   input.value = "";
+// }
 
-messageForm.addEventListener("submit", handleSubmit);
-nicknameForm.addEventListener("submit", handleNickSubmit);
+// function handleNickSubmit(event) {
+//   event.preventDefault();
+//   const input = nicknameForm.querySelector("input");
+//   socket.send(makeMessage("nickname", input.value));
+//   input.value = "";
+// }
+
+// messageForm.addEventListener("submit", handleSubmit);
+// nicknameForm.addEventListener("submit", handleNickSubmit);
--- src/server.js
+++ src/server.js
@@ -1,7 +1,8 @@
 // Back-End 부분
 import express from "express";
 import http from "http";
-import WebSocket, { WebSocketServer } from "ws";
+// import WebSocket, { WebSocketServer } from "ws";
+import { Server, Socket } from "socket.io";
 
 const app = express();
 
@@ -11,15 +12,27 @@ app.use("/public", express.static(__dirname + "/public"));
 app.get("/", (req, res) => res.render("home"));
 app.get("/*", (_, res) => res.redirect("/"));
 
-const handleListen = () =>
-  console.log(`Listening on http(ws)://localhost:3000`);
+const handleListen = () => console.log(`Listening on http://localhost:3000`);
 
 // 같은 서버에서 http, webSocket 둘 다 작동시키는 준비(꼭 둘다 작동 시켜야하는것은 아니다, 웹소켓서버만 작동해도 상관없음)
 // 여기선 두 서버가 같은 포트에 있으면해서 이렇게 함
 // 서버를 만들고(보이게 노출시키고) 그 다음 http서버 위에 ws서버를 만들기 위함
 //  http서버가 필요한 이유는 views, static files, home, redirection 를 사용하기 위함
-const server = http.createServer(app);
-const wss = new WebSocketServer({ server });
+// const server = http.createServer(app);
+// const wss = new WebSocketServer({ server });
+
+const httpServer = http.createServer(app);
+const wsServer = new Server(httpServer);
+
+// 프론트에서 백으로 연결
+wsServer.on("connection", (socket) => {
+  socket.on("enter_room", (msg, done) => {
+    console.log(msg);
+    setTimeout(() => {
+      done();
+    }, 10000);
+  });
+});
 
 // function handleConnection(socket) {
 //   console.log(socket);
@@ -35,43 +48,43 @@ function onSocketClose() {
 //   console.log(message.toString("utf8"));
 // }
 // 연결된 브라우저를 저장해둠
-const sockets = [];
+// const sockets = [];
 
-// 여기서의 socket은 연결된 브라우저를 뜻함
-wss.on("connection", (socket) => {
-  sockets.push(socket);
-  socket["nickname"] = "알수없음";
-  console.log("Connected to Browser ✅");
-  // browser를 닫으면 close 이벤트 발생
-  socket.on("close", onSocketClose);
-  // socket.on("close", () => {
-  //   console.log("Disconnected from Browser ❌");
-  // });
-  // 연결된 모든 브라우저에 메세지 전송
-  socket.on("message", (msg) => {
-    const message = JSON.parse(msg);
-    // switch문과 if~else문 두가지 방법으로 가능
-    switch (message.type) {
-      case "new_message":
-        sockets.forEach((aSocket) =>
-          aSocket.send(`${socket.nickname}: ${message.payload}`)
-        );
-      case "nickname":
-        socket["nickname"] = message.payload;
-    }
-    // if (parsed.type === "new_message") {
-    //   sockets.forEach((aSocket) => aSocket.send(parsed.payload));
-    // } else if (parsed.type === "nickname") {
-    //   console.log(parsed.payload);
-    // }
-  });
-  // socket.on("message", (message) => {
-  //   console.log(message.toString("utf8"));
-  // });
+// // 여기서의 socket은 연결된 브라우저를 뜻함
+// wss.on("connection", (socket) => {
+//   sockets.push(socket);
+//   socket["nickname"] = "알수없음";
+//   console.log("Connected to Browser ✅");
+//   // browser를 닫으면 close 이벤트 발생
+//   socket.on("close", onSocketClose);
+//   // socket.on("close", () => {
+//   //   console.log("Disconnected from Browser ❌");
+//   // });
+//   // 연결된 모든 브라우저에 메세지 전송
+//   socket.on("message", (msg) => {
+//     const message = JSON.parse(msg);
+//     // switch문과 if~else문 두가지 방법으로 가능
+//     switch (message.type) {
+//       case "new_message":
+//         sockets.forEach((aSocket) =>
+//           aSocket.send(`${socket.nickname}: ${message.payload}`)
+//         );
+//       case "nickname":
+//         socket["nickname"] = message.payload;
+//     }
+// if (parsed.type === "new_message") {
+//   sockets.forEach((aSocket) => aSocket.send(parsed.payload));
+// } else if (parsed.type === "nickname") {
+//   console.log(parsed.payload);
+// }
+// });
+// socket.on("message", (message) => {
+//   console.log(message.toString("utf8"));
+// });
 
-  // send메서드는 서버에 있는게 아닌 socket에 있는 메서드이다
-  // socket으로 data를 보내는 것이다.
-  // socket.send("hello!!!");
-});
+// send메서드는 서버에 있는게 아닌 socket에 있는 메서드이다
+// socket으로 data를 보내는 것이다.
+// socket.send("hello!!!");
+// });
 
-server.listen(3000, handleListen);
+httpServer.listen(3000, handleListen);
